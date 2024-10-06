582e61b30abb3f6b4316e64a0a46d1008c27a324
meseret1918 <meseret1918@gmail.com>
2024-10-06 20:23:24 +0000

    task 100

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,20 +1,14 @@
 import { uploadPhoto, createUser } from './utils';
 
 const asyncUploadUser = async () => {
-    try {
-        const photoResponse = await uploadPhoto();
-        const userResponse = await createUser();
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
 
-        return {
-            photo: photoResponse,
-            user: userResponse,
-        };
-    } catch (error) {
-        return {
-            photo: null,
-            user: null,
-        };
-    }
+    return { photo, user };
+  } catch (error) {
+    return { photo: null, user: null };
+  }
 };
 
 export default asyncUploadUser;
