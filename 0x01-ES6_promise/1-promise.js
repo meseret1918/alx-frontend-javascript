e51aad5b1d8702499cd2bc155042e6c05a7af81a
meseret1918 <meseret1918@gmail.com>
2024-10-06 19:52:49 +0000

    task 1

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,13 +1,13 @@
-const getFullResponseFromAPI = (success) => 
-  new Promise((resolve, reject) => {
-    if (success) {
+/* eslint-disable */
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success === true) {
       resolve({
         status: 200,
-        body: 'Success',
+        body: "Success",
       });
     } else {
-      reject(new Error('The fake API is not working currently'));
+      reject(new Error("The fake API is not working currently"));
     }
   });
-
-export default getFullResponseFromAPI;
+}
