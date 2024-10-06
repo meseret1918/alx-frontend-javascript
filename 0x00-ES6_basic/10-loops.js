61be745dbc0ece00eef7b2fca64d3ba9f0ca6231
meseret1918 <meseret1918@gmail.com>
2024-10-06 19:38:03 +0000

    task 10

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,6 @@
-export default function appendToEachArrayValue(array, appendString) {
-  for (const value of array) { // Use for...of instead of for...in
-    const index = array.indexOf(value);
-    array[index] = appendString + value;
-  }
-
-  return array;
+export default function createReportObject(employeesList) {
+  return {
+    allEmployees: { ...employeesList },
+    getNumberOfDepartments: (employeesList) => Object.keys(employeesList).length,
+  };
 }
