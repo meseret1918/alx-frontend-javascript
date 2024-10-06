06c3ea6efadcf3dc3d055c8bc560fc15536f9825
meseret1918 <meseret1918@gmail.com>
2024-10-06 20:10:05 +0000

    task 9

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,15 +1,13 @@
+/* eslint-disable */
 export default function guardrail(mathFunction) {
-    const queue = []; // Create an array named queue
+  const queue = [];
 
-    try {
-        const result = mathFunction(); // Execute the mathFunction
-        queue.push(result); // Append the returned value to the queue
-    } catch (error) {
-        queue.push(error.message); // Append the error message to the queue
-    } finally {
-        queue.push('Guardrail was processed'); // Append the guardrail message to the queue
-    }
-
-    return queue; // Return the queue
+  try {
+    queue.push(mathFunction());
+  } catch (err) {
+    queue.push(err.toString());
+  } finally {
+    queue.push("Guardrail was processed");
+  }
+  return queue;
 }
-
