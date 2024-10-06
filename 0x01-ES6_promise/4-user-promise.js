536f06eaf21dd1f1f9ca8c9a25396b03ffc20e3c
meseret1918 <meseret1918@gmail.com>
2024-10-06 20:00:32 +0000

    task 4

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,7 +1,5 @@
 export default function signUpUser(firstName, lastName) {
-    return Promise.resolve({
-        firstName: firstName,
-        lastName: lastName,
-    });
+  return new Promise((resolve) => {
+    resolve({ firstName, lastName });
+  });
 }
-
