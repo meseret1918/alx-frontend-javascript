dd6853374c42212ccdb0af861f95f8b85cab0ee3
meseret1918 <meseret1918@gmail.com>
2024-10-06 21:02:12 +0000

    task 9

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -1,22 +1,50 @@
-export default class HolbertonClass {
-  constructor(size, location) {
-    this._size = size;
+// Export the HolbertonClass first
+export class HolbertonClass {
+  constructor(year, location) {
+    this._year = year;
     this._location = location;
   }
 
-  get size() {
-    return this._size;
+  get year() {
+    return this._year;
   }
 
   get location() {
     return this._location;
   }
+}
 
-  valueOf() {
-    return this._size;
+// Create class instances after defining HolbertonClass
+const class2019 = new HolbertonClass(2019, 'San Francisco');
+const class2020 = new HolbertonClass(2020, 'San Francisco');
+
+// Correct the constructor of StudentHolberton
+export class StudentHolberton {
+  constructor(firstName, lastName, holbertonClass) {
+    this._firstName = firstName;
+    this._lastName = lastName;
+    this._holbertonClass = holbertonClass; // Use the parameter instead of an undefined variable
   }
 
-  toString() {
-    return this._location;
+  get fullName() {
+    return `${this._firstName} ${this._lastName}`;
+  }
+
+  get holbertonClass() {
+    return this._holbertonClass; // Correctly access the class instance
+  }
+
+  get fullStudentDescription() {
+    return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`; // Use _holbertonClass
   }
 }
+
+// Create Student instances after defining StudentHolberton
+const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
+const student2 = new StudentHolberton('John', 'Doe', class2020);
+const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
+const student4 = new StudentHolberton('Donald', 'Bush', class2019);
+const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
+
+// Create a list of students
+export const listOfStudents = [student1, student2, student3, student4, student5];
