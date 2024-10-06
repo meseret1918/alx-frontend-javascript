5f6ee8a54f27327cd3426ece64ba40a35ecfa8f0
meseret1918 <meseret1918@gmail.com>
2024-10-06 16:26:37 +0000

    update

--- 0x00-ES6_basic/.eslintrc.js
+++ 0x00-ES6_basic/.eslintrc.js
@@ -1,19 +1,35 @@
 module.exports = {
   env: {
-    browser: true,
-    es2021: true,
-    node: true,
-    jest: true
+    browser: false,
+    es6: true,
+    jest: true,
   },
   extends: [
-    'eslint:recommended'
+    'airbnb-base',
+    'plugin:jest/all',
   ],
-  parserOptions: {
-    ecmaVersion: 12,
-    sourceType: 'module'
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
   },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['jest'],
   rules: {
-    // Add your custom rules here
-  }
+    'no-console': 'off',
+    'no-shadow': 'off',
+    'no-restricted-syntax': [
+      'error',
+      'LabeledStatement',
+      'WithStatement',
+    ],
+  },
+  overrides:[
+    {
+      files: ['*.js'],
+      excludedFiles: 'babel.config.js',
+    }
+  ]
 };
-
--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,12 +1,11 @@
 export default function taskBlock(trueOrFalse) {
-  const task = false;
-  const task2 = true;
+  let task = false; // Change var to let
+  let task2 = true; // Change var to let
 
   if (trueOrFalse) {
-    /* eslint-disable */
-    task;
-    task2;
+    task = true; // No var or let here since task is already declared
+    task2 = false; // No var or let here since task2 is already declared
   }
-  
+
   return [task, task2];
 }
