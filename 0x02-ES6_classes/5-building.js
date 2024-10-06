7301cc0d1e6921efbbf4c2057abd9b4e06dfd9fd
meseret1918 <meseret1918@gmail.com>
2024-10-06 20:54:32 +0000

    task 5

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,13 +1,26 @@
+/* eslint-disable no-underscore-dangle */
 export default class Building {
+  /**
+   * @param {number} sqft - size of building
+   *
+   * Implement a getter and setter for sqft
+   */
   constructor(sqft) {
-    this._sqft = sqft;
+    this.sqft = sqft;
+    if (this.constructor !== Building) {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error(
+          'Class extending Building must override evacuationWarningMessage',
+        );
+      }
+    }
   }
 
   get sqft() {
     return this._sqft;
   }
 
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
+  set sqft(value) {
+    this._sqft = value;
   }
 }
