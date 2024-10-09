e17b2bb673ccc270e62b3e9eebb32d96b93fb2ac
meseret1918 <meseret1918@gmail.com>
2024-10-09 00:39:58 +0000

    More map data structure

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,7 +1,13 @@
-export default function updateUniqueItems(map) {
-    if (!(map instanceof Map)) throw new Error('Cannot process');
-    map.forEach((value, key) => {
-      if (value === 1) map.set(key, 100);
-    });
-    return map;
+const updateUniqueItems = (items) => {
+  if (!(items instanceof Map)) {
+    throw new Error('Cannot process');
   }
+  for (const [k, v] of items.entries()) {
+    if (v === 1) {
+      items.set(k, 100);
+    }
+  }
+  return items;
+};
+
+export default updateUniqueItems;
