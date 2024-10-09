4dd40881a75cc3a7f34c218ff18b9f6c100ba788
meseret1918 <meseret1918@gmail.com>
2024-10-09 00:17:58 +0000

    Weak link data structure

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,23 +1,13 @@
 export const weakMap = new WeakMap();
 
-export function queryAPI(endpoint) {
-  if (!weakMap.has(endpoint)) weakMap.set(endpoint, 0);
-  weakMap.set(endpoint, weakMap.get(endpoint) + 1);
-
-  if (weakMap.get(endpoint) >= 5) {
-    throw new Error('Endpoint load is high');
+export const queryAPI = (endpoint) => {
+  if (weakMap.has(endpoint)) {
+    const endpointData = weakMap.get(endpoint);
+    if (endpointData >= 4) {
+      throw new Error('Endpoint load is high');
+    }
+    weakMap.set(endpoint, (endpointData + 1));
+  } else {
+    weakMap.set(endpoint, 1);
   }
-}
-const endpoint = { protocol: 'http', name: 'getUsers' };
-weakMap.get(endpoint);
-
-queryAPI(endpoint);
-console.log(weakMap.get(endpoint));
-
-queryAPI(endpoint);
-console.log(weakMap.get(endpoint));
-
-queryAPI(endpoint);
-queryAPI(endpoint);
-queryAPI(endpoint);
-queryAPI(endpoint);
+};
